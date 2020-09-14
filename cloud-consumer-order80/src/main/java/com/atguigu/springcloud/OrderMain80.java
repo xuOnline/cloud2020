b8f15591b6590e11a4c5fa04a454d348ee332da8
xu <15879849205@163.com>
2020-09-14 23:08:03 +0800

    [ADD] 添加微服务消费订单模块

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xujian
+ * @date 2020-09-14 22:38
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
